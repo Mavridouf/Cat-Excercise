6dce5aa494114040616ffde422fe2092f3900e5f
George Mavridis <g.mavridis@pan-net.eu>
2022-11-27 15:35:50 +0200

    Add lazy loading

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
+import React, {
+  Suspense,
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import classes from "./App.module.css";
 import Header from "./components/header/Header";
-import CatsLanding from "./pages/cats-landing/CatsLanding";
-import Breeds from "./pages/breeds/Breeds";
-import Favourites from "./pages/favourites/Favourites";
 import { CatsProvider } from "./context/cats-context";
 import { BreedsProvider } from "./context/breeds-context";
 import { CatDetailsProvider } from "./context/cat-details-context";
 import { ToastsProvider } from "./context/toasts-context";
 import Toasts from "./components/toasts/Toasts";
 import { FavouritesProvider } from "./context/favourites-context";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShieldCat } from "@fortawesome/free-solid-svg-icons";
+
+// In such a small app it might not make sense to implement Route based Lazy Loading
+
+const CatsMainPage = React.lazy(() =>
+  import("./pages/cats-landing/CatsLanding")
+);
+const BreedsPage = React.lazy(() => import("./pages/breeds/Breeds"));
+const FavouritesPage = React.lazy(() =>
+  import("./pages/favourites/Favourites")
+);
+
+const SuspenseFallback = () => {
+  return (
+    <div
+      style={{
+        height: window.innerHeight - 194,
+      }}
+      className={classes["suspense-fallback-container"]}
+    >
+      <FontAwesomeIcon
+        className={classes["suspense-badge-icon"]}
+        icon={faShieldCat}
+        size="6x"
+        beat
+      />
+    </div>
+  );
+};
 
 function App() {
   return (
@@ -17,39 +52,41 @@ function App() {
       <div className="App">
         <Toasts />
         <Header></Header>
-        <div className={classes.container}>
-          <Routes>
-            <Route path="/" element={<Navigate replace to="/cats" />} />
-            <Route
-              path="/cats/*"
-              element={
-                <CatsProvider>
-                  <CatDetailsProvider>
-                    <CatsLanding />
-                  </CatDetailsProvider>
-                </CatsProvider>
-              }
-            ></Route>
-            <Route
-              path="/breeds/*"
-              element={
-                <BreedsProvider>
+        <Suspense fallback={<SuspenseFallback />}>
+          <div className={classes.container}>
+            <Routes>
+              <Route path="/" element={<Navigate replace to="/cats" />} />
+              <Route
+                path="/cats/*"
+                element={
                   <CatsProvider>
-                    <Breeds />
+                    <CatDetailsProvider>
+                      <CatsMainPage />
+                    </CatDetailsProvider>
                   </CatsProvider>
-                </BreedsProvider>
-              }
-            ></Route>
-            <Route
-              path="/favourites"
-              element={
-                <FavouritesProvider>
-                  <Favourites />
-                </FavouritesProvider>
-              }
-            ></Route>
-          </Routes>
-        </div>
+                }
+              ></Route>
+              <Route
+                path="/breeds/*"
+                element={
+                  <BreedsProvider>
+                    <CatsProvider>
+                      <BreedsPage />
+                    </CatsProvider>
+                  </BreedsProvider>
+                }
+              ></Route>
+              <Route
+                path="/favourites"
+                element={
+                  <FavouritesProvider>
+                    <FavouritesPage />
+                  </FavouritesProvider>
+                }
+              ></Route>
+            </Routes>
+          </div>
+        </Suspense>
       </div>
     </ToastsProvider>
   );
